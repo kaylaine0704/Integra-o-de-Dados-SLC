98f39ceddbc5d078952f83a21759bdb69babf267
Kaylaine Santos <k.santos@cerensa.com>
2025-09-08 10:52:50 -0300

    Remove all files from repository

--- criar_banco.sql
+++
@@ -1,6 +0,0 @@
--- Script para criar o banco de dados todolist no MySQL
-CREATE DATABASE IF NOT EXISTS todolist;
-USE todolist;
-
--- Verificar se as tabelas foram criadas automaticamente pelo Hibernate
-SHOW TABLES;
--- src/main/java/com/meuprojeto/todolist/enums/Status.java
+++
@@ -1,7 +0,0 @@
-package com.meuprojeto.todolist.enums;
-
-public enum Status {
-    PENDENTE,
-    EM_ANDAMENTO,
-    CONCLUIDA
-}
